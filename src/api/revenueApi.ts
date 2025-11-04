cdd915a8a9ffc5a7dc6b9519409cbe13d3409d80
Okiki Oriola <Kikitech@Okikis-MacBook-Pro.local>
2025-11-04 11:42:35 +0100

    latest changes

--- src/api/revenueApi.ts
+++ src/api/revenueApi.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// const API_BASE = "https://fe-task-api.mainstack.io";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const fetchUserData = async () => {
   const res = await axios.get(`${API_BASE_URL}/user`);
